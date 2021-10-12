5d506dff306decfbbc6104712303f95182d6f970
Robert Palermo <robert.palermo.official@gmail.com>
2021-10-12 13:55:10 -0400

    Added Bullet Direction and Player Bullet

--- Assets/[Scripts]/BulletBehaviour.cs
+++ Assets/[Scripts]/BulletBehaviour.cs
@@ -4,26 +4,58 @@ using UnityEngine;
 
 public class BulletBehaviour : MonoBehaviour
 {
+    [Header("Bullet Movement")]
     [Range(0.0f, 2.0f)]
     public float speed;
-    public BulletManager bulletManager;
     public Bound bulletBounds;
+    public BulletDirection direction;
+
+    private BulletManager bulletManager;
+    private Vector3 bulletVelocity;
 
     private void Start()
     {
         bulletManager = FindObjectOfType<BulletManager>();
+
+        switch(direction)
+        {
+            case BulletDirection.UP:
+                bulletVelocity = new Vector3(0.0f, speed, 0.0f);
+                break;
+            case BulletDirection.RIGHT:
+                bulletVelocity = new Vector3(speed, 0.0f, 0.0f);
+                break;
+            case BulletDirection.DOWN:
+                bulletVelocity = new Vector3(0.0f, -speed, 0.0f);
+                break;
+            case BulletDirection.LEFT:
+                bulletVelocity = new Vector3(-speed, 0.0f, 0.0f);
+                break;
+        }
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
-        transform.position -= new Vector3(0, speed, 0);
+        Move();
         CheckBounds();
+    }
+
+    private void Move()
+    {
+        transform.position += bulletVelocity;
     }
 
     public void CheckBounds()
     {
+        // Checks Bottom Bounds
         if (transform.position.y < bulletBounds.max)
+        {
+            bulletManager.ReturnBullet(gameObject);
+        }
+
+        // Checks Top Bounds
+        if (transform.position.y > bulletBounds.min)
         {
             bulletManager.ReturnBullet(gameObject);
         }
